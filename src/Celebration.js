0288eceb189b6b344f69d84c0f990b662479c5cd
Dharani Dharan D <121165539+dharani2812@users.noreply.github.com>
2025-03-23 20:31:05 +0530

    Add files via upload

---
+++ src/Celebration.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Celebration.css"; // Import CSS for animation
+
+function Celebration() {
+  return (
+    <div className="celebration">
+      <span className="bird">🕊️</span>
+      <span className="bird">🦜</span>
+      <span className="bird">🐦</span>
+      <span className="bird">🦢</span>
+      <span className="flower">🌸</span>
+      <span className="flower">🌺</span>
+      <span className="flower">🌻</span>
+      <span className="flower">🌼</span>
+      <span className="bird">🦉</span>
+      <span className="bird">🦅</span>
+    </div>
+  );
+}
+
+export default Celebration;
